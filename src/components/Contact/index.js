ea9d1b3347b34faaf85891884a161faac743f44d
hamomgs <hamomgomer947@gmail.com>
2022-10-04 15:34:07 -0300

    💄 feat: Form removed and image added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import * as S from './styles.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faMap } from '@fortawesome/free-regular-svg-icons'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import ContactImg from '../../assets/contact-image.png'
 
 export default function Contact() {
-  return (
+ return (
     <S.ContactSection>
       <S.ContactContainer>
         <S.Title>Entre em contato com a gente!</S.Title>
@@ -16,35 +18,12 @@ export default function Contact() {
           <S.Icon icon={faEnvelope}></S.Icon>
           contato@petcenter.com
         </S.P>
+        <S.ContactLink href='https://api.whatsapp.com/send?phone=5531978649999' title='Entre em contato pelo whatsapp'>
+            <FontAwesomeIcon icon={faWhatsapp} ></FontAwesomeIcon>
+            Agende uma consulta
+          </S.ContactLink>
       </S.ContactContainer>
-
-      <S.FormContainer onSubmit={e => e.preventDefault()}>
-        <S.FormTitle>
-          <S.Icon icon={faEnvelope}></S.Icon>
-          Preencha o formulário
-        </S.FormTitle>
-        <S.InputsContainer>
-          <S.Label for='firstName'>Primeiro nome
-          <S.Input type='text' id='firstName' name='firstName' placeholder='Primeiro nome' required /></S.Label>
-
-          <S.Label for='lastName'>Último nome
-          <S.Input type='text' id='lastName' name='lastName' placeholder='Último nome' required /></S.Label>
-
-          <S.Label for='whatsapp'>Seu Whatsapp
-          <S.Input mask='(99) 99999-9999' type='tel' id='whatsapp' name='whatsapp' placeholder='(00) 00000-0000' required /></S.Label>
-
-          <S.Label for='email'>Email
-          <S.Input type='email' id='email' name='email' placeholder='exemplo@email.com' required /></S.Label>
-
-          <S.Label for='message' className='textArea'> Mensagem
-          <S.Textarea id='message' placeholder='Digite sua mensagem...' minLength='20' maxLength='300' required /></S.Label>
-
-          <S.SendBtn>
-            Enviar
-            <S.Icon icon={faArrowRight} className='btnIcon'></S.Icon>
-          </S.SendBtn>
-        </S.InputsContainer>
-      </S.FormContainer>
+      <S.Image src={ContactImg} alt="Homem usando celular" />
     </S.ContactSection>
   )
 }
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import InputMask from 'react-input-mask'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const ContactSection = styled.section`
@@ -9,7 +8,7 @@ export const ContactSection = styled.section`
   gap: 68px;
   max-width: 1200px;
   margin: 0 auto;
-  padding: 150px 25px 0 25px;
+  padding: 150px 25px 150px 25px;
   transform: scale(1.1);
 `
 
@@ -18,10 +17,10 @@ export const ContactContainer = styled.div`
 `
 
 export const Icon = styled(FontAwesomeIcon)`
-  color: #00856F;
+  color: var(--primary-color);
 
   &.btnIcon {
-    color: #DCE9E2;
+    color: var(--secondary-color);
   }
 `
 
@@ -42,99 +41,32 @@ export const P = styled.p`
   padding-bottom: 16px;
 `
 
-export const FormContainer = styled.form`
+export const ContactLink = styled.a`
   display: flex;
-  flex-direction: column;
-  gap: 36px;
-  width: 455px;
-  padding: 50px 0;
-`
-
-export const FormTitle = styled.h3`
-  display: flex;
+  justify-content: center;
   align-items: center;
   gap: 16px;
-  font-size: 20px;
-  color: #212529;
-`
-
-export const InputsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-`
-
-export const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  width: 210px;
+  width: 250px;
+  height: 50px;
+  margin-top: 35px;
+  border: solid 2px var(--primary-color);
+  border-radius: 40px;
+  text-transform: uppercase;
   font-size: 14px;
-  color: #00453A;
-
-  &.textArea {
-    width: 356px;
-    height: 120px;
-  }
-`
-
-export const Input = styled(InputMask)`
-  width: 100%;
-  height: 34px;
-  border: solid 0.6px #4950574D;
-  border-radius: 2.4px;
-  padding: 0 10px;
-  color: #212529;
-  background-color: #DCE9E2;
-
-  &::placeholder{
-    color: #49505780;
-  }
-
-  &:focus {
-    border-color: #00856F;
-  }
-`
-
-export const Textarea = styled.textarea`
-  width: 436px;
-  height: 100%;
-  resize: none;
-  border: solid 0.6px #4950574D;
-  border-radius: 2.4px;
-  padding: 10px;
-  color: #212529;
-  background-color: #DCE9E2;
-
-  &::-webkit-scrollbar {
-    width: 5px;
-  }
-
-  &::-webkit-scrollbar-track {
-    background: #DCE9E2;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background: #00856F;
-  }
-
-  &::placeholder{
-    color: #49505780;
-  }
-
-  &:focus {
-    border-color: #00856F;
-  }
-`
-
-export const SendBtn = styled.button`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  padding: 10px 15px;
-  border: none;
-  color: #DCE9E2;
   font-weight: bold;
-  cursor: pointer;
-  background-color: #00856F;
+  color: #fff;
+  transition: all 0.5s ease;
+  background-color: var(--primary-color);
+
+  :hover {
+    background-color: transparent;
+    color: var(--primary-color);
+  }
+
+  * {
+    font-size: 20px;
+  }
+`
+
+export const Image = styled.img`
 `
