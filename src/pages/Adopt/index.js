6e6a616d0681bedca22b87b11f96144eda6a4a53
hamomgs <hamomgomer947@gmail.com>
2022-10-04 17:24:03 -0300

    ✨ feat: Adopt page

--- src/pages/Adopt/index.js
+++ src/pages/Adopt/index.js
@@ -1,13 +1,131 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as S from './styles.js'
+import { dogInfo } from '../../PetsInfos/DogsInfo.js'
+import { catInfo } from '../../PetsInfos/CatsInfo.js'
+import 'react-dropdown/style.css'
+import SearchLoader from '../../components/SearchLoader/index.js'
+import { ThreeDots } from 'react-loader-spinner'
 
 export default function Adopt() {
+  const [image, setImage] = useState(['', '', ''])
+  const [randomNumber, setRandomNumber] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+  const [pet, setPet] = useState('dog')
+  const [test, setTest] = useState(true)
+  const [petGender, setPetGender] = useState('ambos')
+  const [petInfo, setPetInfo] = useState(pet === 'dog' ? dogInfo : catInfo)
+
   useEffect(() => {
     document.querySelector('#adopt').classList.add('selected')
-  }, [])
+
+    const apiUrl = pet === 'dog' ? `https://dog.ceo/api/breed/mix/images/random/3` : `https://api.thecatapi.com/v1/images/search?limit=3`
+
+    if (test && pet === 'dog') {
+      if(petGender === 'feminino') {
+        setPetInfo(dogInfo.filter(item => item.gender === 'Feminino'))
+      } else if(petGender === 'masculino') {
+        setPetInfo(dogInfo.filter(item => item.gender === 'Masculino'))
+      } else {
+        setPetInfo(dogInfo)
+      }
+    } else if (test && pet === 'cat') {
+      if(petGender === 'feminino') {
+        setPetInfo(catInfo.filter(item => item.gender === 'Feminino'))
+      } else if(petGender === 'masculino') {
+        setPetInfo(catInfo.filter(item => item.gender === 'Masculino'))
+      } else {
+        setPetInfo(catInfo)
+      }
+    }
+
+    if(test) {
+      setTest(false)
+      setIsLoading(true)
+
+      axios.get(apiUrl)
+      .then(res => {
+        setImage(pet === 'dog' ? res.data.message : res.data.map((item) => item.url))
+        setRandomNumber(Math.floor(Math.random() * (petInfo.length - 13)))
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 1500)  
+      })
+      .catch(err => console.log("Something don't work", err))
+    }
+  }, [test, petInfo, pet, petGender])
+  
+  function handleSearchNewDog(e) {
+    setTimeout(() => {
+      if (!e.target.classList.contains('loading')) {
+        e.target.classList.add('loading')
+        setTest(true)
+
+        setTimeout(() => {
+          e.target.classList.remove('loading')
+        }, 2500)
+      }
+    }, 200)
+  }
+
+  const options = ['Ambos', 'Feminino', 'Masculino']
 
   return (
-    <div>
-      <h1>Adoção</h1>
-    </div>
+    <S.AdoptSection>
+      <S.SearchContainer>
+        <S.FilterTitle>FILTRO:</S.FilterTitle>
+        <S.FilterContainer>
+          <S.PetChoseContainer>
+            <S.PetInput type='radio' onClick={() => setPet('dog')} name='pet' id='dog' defaultChecked />
+            <S.PetLabel htmlFor='dog'>Cachorro</S.PetLabel>
+          
+            <S.PetInput type='radio' onClick={() => setPet('cat')} name='pet' id='cat' />
+            <S.PetLabel htmlFor='cat'>Gato</S.PetLabel>
+          </S.PetChoseContainer>
+          <S.DropdownMenu id='dropdown' options={options} onChange={e => setPetGender(e.value.toLowerCase())}  placeholder='Sexo do pet' />
+        </S.FilterContainer>
+        <S.SearchBtn onClick={e => handleSearchNewDog(e)}>BUSCAR</S.SearchBtn>
+      </S.SearchContainer>
+
+      <S.CardsContainer>
+        {image.map((item, index) => (
+          <S.PetCard key={index}>
+          <S.ImageContainer>
+            {isLoading ? <SearchLoader /> :
+              <a href={image[index]} target='_blank' rel='noreferrer'>
+                <S.PetImage src={item} alt='Foto de cachorro para adoção' />
+              </a>
+            }
+            </S.ImageContainer>
+            <S.PetDescription>
+            {isLoading ? <ThreeDots color='#00453A' width='40' /> :
+              <S.InformationList>
+                <S.PetInformation>
+                <span>Nome:</span> {petInfo[randomNumber + index].name}
+              </S.PetInformation>
+              <S.PetInformation>
+                <span>Idade:</span> {petInfo[randomNumber + index].age} ano(s) 
+              </S.PetInformation>
+              <S.PetInformation>
+                <span>Sexo:</span> {petInfo[randomNumber + index].gender}
+              </S.PetInformation>
+              <S.PetInformation>
+                <span>Raça:</span> Não definida
+              </S.PetInformation>
+              </S.InformationList>
+            }
+            </S.PetDescription>
+          <S.BtnContainer>
+            <S.AdoptBtn>
+              ADOTAR
+            </S.AdoptBtn>
+            <S.PatronizeBtn>
+              APADRINHAR
+            </S.PatronizeBtn>
+          </S.BtnContainer>
+        </S.PetCard>
+        ))}
+      </S.CardsContainer>
+    </S.AdoptSection>
   )
 }
--- src/pages/Adopt/styles.js
+++ src/pages/Adopt/styles.js
@@ -1,1 +1,193 @@
 import styled from 'styled-components'
+import LoadingBg from '../../assets/loading-bg.gif'
+import Dropdown from 'react-dropdown'
+
+export const AdoptSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 25px;
+`
+
+export const DropdownMenu = styled(Dropdown)`
+  .Dropdown-control {
+    width: 155px;
+    cursor: pointer;
+    border-radius: 8px;
+    border-color: var(--primary-color); 
+  }
+
+  .Dropdown-menu {
+    border-radius: 8px;
+  }
+
+  .Dropdown-option{
+    :hover {
+      background-color: var(--secondary-color);
+    }
+  }
+`
+
+export const SearchBtn = styled.button`
+  width: 150px;
+  padding: 10px 0;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.2s;
+  background-color: var(--primary-color);
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &.loading {
+    opacity: 0.5;
+    cursor: wait;
+  }
+`
+
+export const CardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin: 20px 0;
+`
+
+export const PetCard = styled.div`
+  height: 100%;
+  padding: 24px;
+  border-radius: 6px;
+  border: solid 2px var(--secondary-color);
+  box-shadow: 0 0 2px var(--primary-color);
+`
+
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
+export const PetImage = styled.img`
+  background-image: url(${LoadingBg});
+  background-position: center;
+  background-size: 80%;
+  background-repeat: no-repeat;
+  width: 240px;
+  min-height: 240px;
+  max-height: 240px;
+  border-radius: 6px;
+  border: solid 3px var(--primary-color);
+  background-color: #fff;
+`
+
+export const PetDescription = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 137px;
+  font-weight: bold;
+  color: #212529;
+`
+
+export const InformationList = styled.ul`
+  width: 100%;
+  min-height: 97px;
+  margin: 20px 0;
+`
+
+export const PetInformation = styled.li`
+  margin: 2.5px 0;
+  font-weight: 500;
+
+  span {
+    font-weight: bold;
+  }
+`
+
+export const BtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  button {
+    width: 240px;
+    margin: 0 auto;
+    padding: 10px 0;
+    color: #fff;
+    font-weight: bold;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  `
+
+export const AdoptBtn = styled.button`
+  background-color: var(--primary-color);
+  border-radius: 3px 3px 0 0;
+`
+
+export const PatronizeBtn = styled.button`
+  background-color: #5EAF9F;
+  border-radius: 0 0 3px 3px;
+`
+
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  width: 80%;
+  margin-top: 20px;
+`
+
+export const FilterTitle = styled.h4`
+  color: var(--primary-color);
+`
+
+export const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  input {
+    display: none;
+  }
+`
+
+export const PetChoseContainer = styled.div`
+  display: flex;
+  gap: 15px;
+`
+
+export const PetLabel = styled.label`
+  cursor: pointer;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    margin: 0 5px -3.5px 0;
+    border-radius: 50%;
+    border: solid 1px var(--primary-color);
+    cursor: pointer;
+    transition: all 0.2s;
+  }
+`
+
+export const PetInput = styled.input`
+  &:checked + label::before {
+    background-color: var(--primary-color);
+  }
+`
